51f326f14c78f7b93d6c92a89ad131406a17c540
MR DOAN <doantrongvinhse@gmail.com>
2026-05-23 16:25:00 +0700

    update api

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://taycambe.onrender.com';
+export const API_BASE_URL = 'https://chotdon.site';
 
 export const API_ENDPOINTS = {
   PRODUCTS: `${API_BASE_URL}/api/v1/products`,
