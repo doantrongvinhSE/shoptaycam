d82b49bf2d76004dafe79fa3f485a5286a14f786
MR DOAN <doantrongvinhse@gmail.com>
2026-05-29 19:46:24 +0700

    update api

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://chotdon.site';
+export const API_BASE_URL = 'https://chotdot.site';
 
 export const API_ENDPOINTS = {
   PRODUCTS: `${API_BASE_URL}/api/v1/products`,
